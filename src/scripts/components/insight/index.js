5bfdcdc4e3d3d58e96bfbee98a33bf4a4e90767c
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-09-02 19:53:45 +0700

    create insight component

---
+++ src/scripts/components/insight/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './insight.scss';
+
+const Insight = () => (
+  <div className="insights container">
+    <p>Ready to work your project? Let’s get to work.</p>
+    <button type="submit">Let`s Work Togeter</button>
+  </div>
+);
+
+export default Insight;
