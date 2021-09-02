8d3e15797e1bdb30a42e20c0b9ee3872d282c28a
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-09-02 20:31:26 +0700

    style main and implement component in app.js

---
+++ src/scripts/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/header';
+import Hero from './components/hero';
+import Projects from './components/projects';
+import Clients from './components/clients';
+import Teams from './components/teams';
+import Insights from './components/insight';
+import Footer from './components/footer';
+
+const App = () => (
+  <>
+    <Header />
+    <Hero />
+    <Projects />
+    <Clients />
+    <Teams />
+    <Insights />
+    <Footer />
+  </>
+);
+
+export default App;
